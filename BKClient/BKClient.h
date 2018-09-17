10e1ceea5bf3d4e3d6e1072f2d1d7bfb61456c9c
ys89 <ys89@clay-fingers.com>
2018-09-17 15:10:31 +0900

    Initial Commit

---
+++ BKClient/BKClient.h
@@ -0,0 +1,19 @@
+//
+//  BKClient.h
+//  BKClient
+//
+//  Created by CLAYFINGERS on 17/09/2018.
+//  Copyright © 2018 Bunker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BKClient.
+FOUNDATION_EXPORT double BKClientVersionNumber;
+
+//! Project version string for BKClient.
+FOUNDATION_EXPORT const unsigned char BKClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BKClient/PublicHeader.h>
+
+
